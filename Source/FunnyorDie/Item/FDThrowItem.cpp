145617b64bc5f9732416425203e3a57442599a14
brogdam <brogdam208@kakao.com>
2026-07-09 13:06:15 +0900

    <백록담> [add] 투척 아이템 클래스 생성

---
+++ Source/FunnyorDie/Item/FDThrowItem.cpp
@@ -0,0 +1,9 @@
+// FDThrowItem.cpp
+
+#include "Item/FDThrowItem.h"
+
+AFDThrowItem::AFDThrowItem()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
---
+++ Source/FunnyorDie/Item/FDThrowItem.h
@@ -0,0 +1,16 @@
+// FDThrowItem.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FDThrowItem.generated.h"
+
+UCLASS()
+class FUNNYORDIE_API AFDThrowItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AFDThrowItem();
+};
